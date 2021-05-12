0c093765d73f1552d1e56468d70857669be2a0c7
Franek Borowiec <franek.borowiec@gmail.com>
2021-05-12 15:36:48 +0200

    Added possibility to extrapolate new gps coordinates

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # **Distance between two gps coordinates**
 
 For an easy calculation of the distance in meters between two gps coordinates.
+
+Once you put the two GPS coordinates you can choose a desired distance from the starting point and the script will extrapolate the new GPS coordinates for you. *NOTE*: Good only for short distances / straight lines!
+
 If you want another planet just change the hardcoded radius of 6371 meters! :)
 
 *How to run:*
--- distance_between_two_gps_coordinates.cpp
+++ distance_between_two_gps_coordinates.cpp
@@ -1,6 +1,7 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -8,13 +9,15 @@
 // C++ program to calculate Distance
 // Between Two Points on Earth
 
-long double DegToRadians(const long double degree) {
+long double DegToRadians(const long double degree)
+{
   long double one_deg = (M_PI) / 180;
   return (one_deg * degree);
 }
 
 long double Distance(long double lat1, long double long1, long double lat2,
-                     long double long2) {
+                     long double long2)
+{
   lat1 = DegToRadians(lat1);
   long1 = DegToRadians(long1);
   lat2 = DegToRadians(lat2);
@@ -27,7 +30,7 @@ long double Distance(long double lat1, long double long1, long double lat2,
   long double ans =
       2 * std::asin(std::sqrt(std::pow(std::sin(dlat / 2), 2) +
                               std::cos(lat1) * cos(lat2) *
-                                  std::pow(std::sin(dlong / 2), 2)));
+                              std::pow(std::sin(dlong / 2), 2)));
 
   // Radius of Earth in
   // Kilometers, R = 6371
@@ -36,11 +39,22 @@ long double Distance(long double lat1, long double long1, long double lat2,
   // Calculate the result
   ans = ans * R;
 
+  // Answer in meters
   return ans * 1000;
 }
 
+std::pair<long double, long double> CalculateDesiredCoordinates(const long double desired_distance,
+                                                                const long double actual_distance,
+                                                                const std::vector<long double> &coordinates)
+{
+  const long double lat = coordinates[0] + (coordinates[2] - coordinates[0]) * desired_distance / actual_distance;
+  const long double lon = coordinates[1] + (coordinates[3] - coordinates[1]) * desired_distance / actual_distance;
+  return {lat, lon};
+}
+
 // Driver Code
-int main() {
+int main()
+{
   std::string latlon1, latlon2;
   std::cout << "FROM coordinates (lat,lon): ";
   std::cin >> latlon1;
@@ -50,17 +64,30 @@ int main() {
   std::stringstream ss{latlon1 + "," + latlon2};
   std::vector<long double> coordinates;
 
-  while (ss.good()) {
+  while (ss.good())
+  {
     std::string substr;
     std::getline(ss, substr, ',');
     coordinates.push_back(static_cast<long double>(stold(substr)));
   }
 
-  // call the distance function
-  // std::cout << setprecision(15) << fixed;
-  std::cout << Distance(coordinates[0], coordinates[1], coordinates[2],
-                        coordinates[3])
-            << " m";
+  // Call the distance function
+  std::cout << std::setprecision(15) << std::fixed;
+  auto actual_distance = Distance(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+  std::cout << std::setprecision(3) << std::fixed;
+  std::cout << actual_distance << " m" << std::endl;
 
+  // Choose a desired distance from the starting points
+  std::string des_dist;
+  std::cout << "Desired distance from starting point: ";
+  std::cin >> des_dist;
+
+  auto desired_distance = static_cast<long double>(stold(des_dist));
+
+  // Calculating the new coordinates
+  const auto new_coordinates = CalculateDesiredCoordinates(desired_distance, actual_distance, coordinates);
+
+  std::cout << std::setprecision(15) << std::fixed;
+  std::cout << "The new coordinates are: " << new_coordinates.first << "," << new_coordinates.second << std::endl;
   return 0;
 }
